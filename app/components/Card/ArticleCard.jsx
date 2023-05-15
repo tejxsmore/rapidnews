ab277dfd6a6f8826f8be81574d6ba2717ec4b5b5
tejxsmore <tejasmore1104@gmail.com>
2023-05-15 19:19:17 +0530

    Updated Article styles

--- app/components/Card/ArticleCard.jsx
+++ app/components/Card/ArticleCard.jsx
@@ -2,11 +2,15 @@ import Link from "next/link";
 
 export default function Content({ title, content, url, img }) {
   return (
-    <div className="m-2">
-      <h1 className="">{title}</h1>
-      <img src={img} alt="Image" style={{ width: 500 }} />
-      <h6 className="">{content}</h6>
-      <Link href={url}>Read more</Link>
+    <div className="flex justify-center">
+      <div className="bg-green m-2 p-2 md:w-96 w-full rounded-lg">
+        <h1 className="text-2xl font-medium py-2">{title}</h1>
+        <img src={img} alt="Image" className="rounded-lg py-2" />
+        <h6 className="py-2">{content}</h6>
+        <Link href={url} className="bg-green">
+          Read more 🔗
+        </Link>
+      </div>
     </div>
   );
 }
