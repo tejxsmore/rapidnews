c073108f92f274ef03f4e3505b298047583bb9fe
tejxsmore <tejasmore1104@gmail.com>
2023-05-25 11:41:01 +0530

    Date time fixed

--- app/components/Element/Navbar.jsx
+++ app/components/Element/Navbar.jsx
@@ -2,7 +2,9 @@ import Label from "./Label";
 
 export default async function Navbar() {
   // const date = new Date().toDateString();
-  const time = new Date().toLocaleTimeString();
+  const date = await fetch(`http://worldtimeapi.org/api/timezone/Asia/Kolkata`);
+  const { datetime } = await date.json();
+
   const data = await fetch(
     `https://api.open-meteo.com/v1/forecast?latitude=19.076090&longitude=72.877426&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m`
   );
@@ -35,7 +37,7 @@ export default async function Navbar() {
             alt="India"
             className="h-6 mr-3 rounded-md"
           />
-          <h1 className="">{time}</h1>
+          <h1 className="">{datetime.slice(0, 10)}</h1>
         </div>
         <div className="flex">
           <img src="img/location.jpg" alt="pin" className="h-6" />
